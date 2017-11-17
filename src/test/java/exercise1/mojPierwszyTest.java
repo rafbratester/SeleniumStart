3fe20730792584a4ea22f2d207a731d3a268db60
rafbratester <rafbra@op.pl>
2017-11-17 17:02:09 +0100

    Zmiany na koniec szkolenia

--- src/test/java/exercise1/mojPierwszyTest.java
+++ src/test/java/exercise1/mojPierwszyTest.java
@@ -6,14 +6,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -21,21 +23,58 @@ public class mojPierwszyTest {
     WebDriver driver;
 
     @BeforeTest
-    public void setUp(){
+    public void setUp()throws MalformedURLException{
      System.setProperty("webdriver.chrome.driver", "c:\\pliki\\chromedriver.exe");
- //       System.setProperty("webdriver.geko.driver", "c:\\pliki\\gekodriver.exe");
+   //    System.setProperty("webdriver.gecko.driver", "c:\\pliki\\geckodriver.exe");
   //      System.setProperty("webdriver.ie.driver", "c:\\pliki\\IEDriverServer.exe");
  //       System.setProperty("webdriver.edge.driver", "c:\\pliki\\MicrosoftWebDriver.exe");
+        DesiredCapabilities chromeCaps = DesiredCapabilities.chrome();
+        chromeCaps.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+        chromeCaps.setCapability(CapabilityType.VERSION, "62.w10");
+        chromeCaps.setCapability(CapabilityType.PLATFORM, "WIN10");
 
-        driver = new ChromeDriver();
-//          driver = new FirefoxDriver();
-//       driver = new InternetExplorerDriver();
- //        driver = new EdgeDriver();
+  //      driver = new ChromeDriver();
+  //      driver = new FirefoxDriver();
+//        driver = new InternetExplorerDriver();
+ //       driver = new EdgeDriver();
+ //           driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+  //      driver.get("http://helion.pl");
 
+        driver = new RemoteWebDriver(new URL("http://212.106.131.203:4444/wd/hub"), chromeCaps);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://helion.pl");
+
+    }
+    @Parameters({"SeleniumHost", "seleniumPort","Browser", "url"})
+    public void setUp (String seleniumHost, int seleniumPort, String browser, String url)
+        throws MalformedURLException {
+        if (browser.equals("firefox")){
+            DesiredCapabilities ffCaps = DesiredCapabilities.firefox();
+            ffCaps.setCapability(CapabilityType.BROWSER_NAME, browser);
+
+            driver = new RemoteWebDriver
+                    (new URL("http//"+seleniumHost+":"+seleniumPort"wd/hub"),ffCaps);
+        }
+        else if (browser.equals("chrome")){
+            DesiredCapabilities chromeCaps = DesiredCapabilities.chrome();
+            chromeCaps.setCapability(CapabilityType.BROWSER_NAME, browser);
+
+            driver = new RemoteWebDriver
+                    (new URL("http//"+seleniumHost+":"+seleniumPort"wd/hub"),chromeCaps);
+        }
+        else if (browser.equals("edge")){
+            DesiredCapabilities edgeCaps = DesiredCapabilities.firefox();
+            edgeCaps.setCapability(CapabilityType.BROWSER_NAME, browser);
+
+            driver = new RemoteWebDriver
+                    (new URL("http//"+seleniumHost+":"+seleniumPort"wd/hub"),edgeCaps);
+        }
+
+        driver.navigate().to(url);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        driver.get("http://helion.pl");
     }
+    
 
     @AfterTest
     public void tearDown() {
@@ -96,7 +135,7 @@ public class mojPierwszyTest {
     @DataProvider
     public Object [][] dataForSearchTest() {
         return new Object[][]{
-                {"selenium", 5}, {"Java", 21}, {"Kali", 4},
+                {"selenium", 3}, {"Java", 21}, {"Kali", 4},
                 {"Jenkins", 3}, {"JavaScript", 20}, {"Git", 10}
 
         };
